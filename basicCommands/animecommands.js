6df5ee80b1722291886b21987d92abd735c76079
Tahirmuzaffer-blip <tahirmuzaffer9@gmail.com>
2024-06-16 05:15:52 +0500

    Update animecommands.js

--- basicCommands/animecommands.js
+++ basicCommands/animecommands.js
@@ -9,7 +9,7 @@ module.exports = {
       .setTitle('Anime Commands')
       .setDescription(`__**✅ List Of Availabe Interations**__\n\n▶️ __**Section 1 :**__\n  blush, cuddle, dance, slap, bonk, bully, hug, confused, kiss, pat, happy, smile.\n\n▶️ __**Section 2 :**__\n yes, highfive, wink, wave, thinking, sad, cry, stare, bored, scream, nervous, kill.\n`)
       
-      .setImage(`https://cdn.discordapp.com/attachments/1140841446228897932/1142126954775068762/pxfuel.jpg`);
+      .setImage(`https://media.discordapp.net/attachments/1165539147881132142/1166348715850735616/6dbd7d91d3fc153376a97682697bb14e107c5f337a992b0ee0c6456f137bf015.gif?ex=666ed6e3&is=666d8563&hm=5517d0fe98cffa76267eb3989c777cf6518f4a33d5e9927d0f35342e50630f51&`);
     message.reply({ embeds: [embed] });
   },
 };
